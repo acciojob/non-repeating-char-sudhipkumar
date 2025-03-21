3c7d77b68e880443c775e0188d8a4868ac1a2806
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-21 08:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ function firstNonRepeatedChar(str) {
     return null;
 }
 
-// const input = prompt("Enter a string");
-// alert(firstNonRepeatedChar(input));
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input));
